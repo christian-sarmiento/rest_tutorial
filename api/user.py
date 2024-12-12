902d7c35dc93910259ad65c31cd6d2d3897bd8a9
rica <rica.e@fcges.com>
2024-12-12 16:52:57 +0800

    implemented PUT and DELETE, added test codes

--- api/user.py
+++ api/user.py
@@ -1,4 +1,4 @@
-from flask_restx import Resource
+from flask_restx import Resource, reqparse
 
 from api.users import users
 
@@ -16,7 +16,39 @@ class User(Resource):
         return {"error": "user not found"}, 404
 
     def put(self, id):
-        pass
+        for user in users:
+            if id == user["id"]:
+                parser = reqparse.RequestParser()
+                parser.add_argument("firstname", type=str, required=True)
+                parser.add_argument("lastname", type=str, required=True)
+                args = parser.parse_args()
+
+                # avoid inserting duplicate user
+                for userb in users:
+                    if id == userb["id"]:
+                        continue
+                    if args["firstname"] != userb["firstname"] or args["lastname"] != userb["lastname"]:
+                        continue
+                    
+                    return {"error": "user already exists!"}, 400
+                
+                # no changes made
+                if user["firstname"] == args["firstname"] and user["lastname"] == args["lastname"]:
+                    return {"msg": "no changes made"}, 200
+                
+                user["firstname"] = args["firstname"]
+                user["lastname"] = args["lastname"]
+
+                return {"msg": "user updated"}, 200
+            
+        return {"error": "user not found"}, 404
+
 
     def delete(self, id):
-        pass
+        for user in users:
+            if id == user["id"]:
+                users.remove(user)
+                return {"msg": "user deleted"}, 200
+            
+        return {"error": "user not found"}, 404
+    
